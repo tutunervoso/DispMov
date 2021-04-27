02d20beddbb182a5890288e99294d23521205cb3
Vicente <louser_master@hotmail.com>
2021-04-26 22:41:44 -0300

    Atulizacao perfil e procurar preparacao gravacao

--- src/app/home/profile/profile-routing.module.ts
+++ src/app/home/profile/profile-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '',
     component: ProfilePage
   },
-  {
+  /*{
     path: 'feed',
     loadChildren: () => import('../feed/feed.module').then( m => m.FeedPageModule)
   },
@@ -19,7 +19,7 @@ const routes: Routes = [
   {
     path: 'likes',
     loadChildren: () => import('../likes/likes.module').then( m => m.LikesPageModule)
-  },
+  },*/
 ];
 
 @NgModule({
