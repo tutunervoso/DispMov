907c41973a234e4597c1322cc1259a5e7569a908
Arthur Neves <arthur14122000@gmail.com>
2021-05-11 23:58:58 -0300

    voltando ln 12

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -14,29 +14,30 @@ const routes: Routes = [
       {
         path: 'people',
         loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+      },
+      {
+        path: 'feed',
+        loadChildren: () => import('./feed/feed.module').then( m => m.FeedPageModule)
+      },
+      {
+        path: 'explore',
+        loadChildren: () => import('./explore/explore.module').then( m => m.ExplorePageModule)
+      },
+      {
+        path: 'likes',
+        loadChildren: () => import('./likes/likes.module').then( m => m.LikesPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+      },
+      {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
       }
     ]
   },
-  {
-    path: 'feed',
-    loadChildren: () => import('./feed/feed.module').then( m => m.FeedPageModule)
-  },
-  {
-    path: 'explore',
-    loadChildren: () => import('./explore/explore.module').then( m => m.ExplorePageModule)
-  },
-  {
-    path: 'likes',
-    loadChildren: () => import('./likes/likes.module').then( m => m.LikesPageModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  }
+ 
 ];
 
 @NgModule({
